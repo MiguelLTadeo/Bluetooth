ccf2cbfcdaa18018dbd6b08a90d9a47445e32d71
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2026-01-18 11:57:21 -0300

    comentários no script de conexão

--- bluetooth-scanner-reader/Bluetooth.gd
+++ bluetooth-scanner-reader/Bluetooth.gd
@@ -1,37 +1,52 @@
 extends Node
 
 var bluetooth_manager: BluetoothManager
+#Nome padrão para conexão
 @export var target_device_name: String = "teste_blu"
 var device: BleDevice
 
 const UUID_SERVICO = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 const UUID_CHAR    = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+#Variável global para acesso a leitura
 @export var posicao: String = "3"
 
+#Função para a conexão ao dispositivo
+
 func conect(nome_disp: String):
+	#cria uma nova instância do nó bluetooth manager
 	bluetooth_manager = BluetoothManager.new()
+	#adição do nó bluetooth manager
 	add_child(bluetooth_manager)
 	
+	#nome a ser usado na conexão
 	if nome_disp!="":
 		target_device_name = nome_disp
 
-	
+	#Com a inicialização do adaptador bluetooth, a função _on_initialized é chamada
 	bluetooth_manager.adapter_initialized.connect(_on_initialized)
+	
+	#Com a descoberta de dispositivos pelo scan, a função _on_device_found é chamada
 	bluetooth_manager.device_discovered.connect(_on_device_found)
+	
+	#Com o fim do scan, a função _on_scan_found é chamada
 	bluetooth_manager.scan_stopped.connect(_on_scan_done)
 	
+	#Inicializa o adaptador bluetooth
 	bluetooth_manager.initialize()
 
 func _on_initialized(success: bool, error: String):
+	#faz um scan de dispositivos por 2 segundos
 	if success:
 		bluetooth_manager.start_scan(2.0)
 
 func _on_device_found(info: Dictionary):
+	#Comparação entre o nome dos dispositivos encontrados com o dispositivo alvo
 	var name = info.get("name", "")
 	if name == target_device_name:
 		print("Dispositivo alvo encontrado!")
 		bluetooth_manager.stop_scan()
+		#Com o dispositivo encontrado a função connect_to_target é chamada
 		connect_to_target(info.get("address"))
 
 
@@ -39,31 +54,43 @@ func _on_scan_done():
 	print("Scan completo.")
 
 func connect_to_target(address: String):
+	#A variável device do tipo BleDevice conecta ao endereço fornecido
 	device = bluetooth_manager.connect_device(address)
 	if device:
+		#Se o dispositivo existe, e está conectado, a função _on_connected é chamada
 		device.connected.connect(_on_connected)
+		
+		#Com os serviços descobertos, a função _on_services_found é chamada
 		device.services_discovered.connect(_on_services_found)
 			
+		#Com novas notificações da característica alvo, a função _on_data_update é chamada
 		device.characteristic_notified.connect(_on_data_update)
+		
+		#Conexão assíncrona ao dispositivo
 		device.connect_async()
 		
 func _on_services_found(services:Array):
+	#Usamos a função subscribe_characteristic para inscrever a notificações 
+	#da característica que temos como alvo
 	print("Serviços encontrados.")
 	device.subscribe_characteristic(UUID_SERVICO, UUID_CHAR)
 
 func _on_data_update(char_uuid: String, data: PackedByteArray):
-	
+	#Tradução da leitura para string
 	if char_uuid.to_lower() == UUID_CHAR.to_lower():
 		var leitura = data.get_string_from_utf8()
 		
+		#Armazenamento global da leitura
 		Bluetooth.posicao = leitura
 		
 		print("Dado recebido: ", leitura)
 
 func _on_connected():
+	#Aqui é realizada a descoberta dos serviços
 	print("Dispositivo Conectado!")
 	device.discover_services()
 
+#Função para desconexão segura do dispositivo e bluetooth manager
 func _exit_tree():
 	if device and device.is_connected():
 		device.disconnect()
